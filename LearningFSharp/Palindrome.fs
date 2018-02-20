c94f773a55b33b9db41c2d5a7d3dd2ee0f87d126
Stephane Larocque <stephl001@gmail.com>
2018-02-20 00:57:00 -0500

    Remove unused code.

--- LearningFSharp/Palindrome.fs
+++ LearningFSharp/Palindrome.fs
@@ -13,9 +13,7 @@ module Palindrome =
         seq {
             let charArray = s |> Array.ofSeq
             for i in [0 .. s.Length/2-1] do
-                let r = (charArray.[i],charArray.[s.Length-i-1])
-                printfn "%A" r
-                yield r
+                yield (charArray.[i],charArray.[s.Length-i-1])
         }
 
     let private isStringSameAsReverse = insideOut >> Seq.forall (fun (x,y) -> x=y)
