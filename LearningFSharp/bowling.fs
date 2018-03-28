dff23f5230de90231933c6052228c9bbdd0ec01f
Stephane Larocque <stephl001@gmail.com>
2018-03-28 07:34:03 -0400

    Refactor bowling kata

--- LearningFSharp/bowling.fs
+++ LearningFSharp/bowling.fs
@@ -1,12 +1,17 @@
 module BowlingKata
 
 type ScoreCard = ScoreCard of int list
+type ScoreGame = ScoreCard -> int
 
 let rec private scoreGameIntern frameId points = function
-| [10;t1;t2] when frameId=10 -> points + 10 + t1 + t2
-| 10::t1::t2::rest -> scoreGameIntern (frameId+1) (points+10+t1+t2) (t1::t2::rest)
-| t1::t2::t3::rest when t1+t2=10 -> scoreGameIntern (frameId+1) (points+10+t3) (t3::rest)
-| t1::t2::rest -> scoreGameIntern (frameId+1) (points+t1+t2) rest
+| [10;t1;t2] when frameId=10 -> 
+    points + 10 + t1 + t2
+| 10::(t1::t2::_ as remainingCard) -> 
+    scoreGameIntern (frameId+1) (points+10+t1+t2) remainingCard
+| t1::t2::(t3::_ as remainingCard) when t1+t2=10 -> 
+    scoreGameIntern (frameId+1) (points+10+t3) remainingCard
+| t1::t2::rest -> 
+    scoreGameIntern (frameId+1) (points+t1+t2) rest
 | _ -> points
 
 let scoreGame (ScoreCard card) = scoreGameIntern 1 0 card
