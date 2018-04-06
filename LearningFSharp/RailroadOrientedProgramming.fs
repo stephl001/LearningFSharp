e8d7135f94d5b49d8b1989632cd55063202c711c
Stephane Larocque <stephl001@gmail.com>
2018-04-06 07:21:36 -0400

    Start RailroadOrientedProgramming

---
+++ LearningFSharp/RailroadOrientedProgramming.fs
@@ -0,0 +1,37 @@
+﻿namespace LearningFSharp
+ 
+module ReadFileSample =
+ 
+    type FileReadError =
+        | FileNotFound of string
+        | FileWrongFormat
+        | FileInvalidLength of int
+ 
+    let readFile = function
+    | "good.txt" -> Ok "CONTENT:This is a super file content!"
+    | "wrongformat.txt" -> Ok "BAD: This is a bad file content!"
+    | filename -> Error (FileNotFound filename)
+ 
+    let readContent (content:string) =
+        if content.StartsWith("CONTENT:")
+        then Ok (content.Substring(8))
+        else Error FileWrongFormat
+ 
+    let transformContent (content:string) =
+        content.ToUpper()
+ 
+    let printContent = function
+    | Ok content -> printfn "Here is the content: %s" content
+    | Error e ->
+        match e with
+        | FileNotFound f -> printfn "The file %s was not found." f
+        | FileWrongFormat -> printfn "The file format is invalid"
+        | FileInvalidLength l -> printfn "The expected file length is 45. Actual length is %d" l
+ 
+    let (>>=) twoTrackInput switchFunction =
+        Result.bind switchFunction twoTrackInput
+    let (>=>) twoTrackInput switchFunction =
+        Result.map switchFunction twoTrackInput
+ 
+    let transformFileContent filename = readFile filename >>= readContent >=> transformContent
+    let printFileContent = transformFileContent >> printContent
---
+++ LearningFSharp/RailroadOrientedProgramming.fsx
@@ -0,0 +1,31 @@
+﻿open System.IO
+
+type GenerationDataHeaderError = 
+    | GenerationIdInvalidFormat
+    | GenerationDimensionsInvalidFormat
+    | GenerationDimensionsOutOfBound
+type ActualLineCount = ActualLineCount of int
+type LineIndex = LineIndex of int
+type ColumnCount = ColumnCount of int
+type GenerationError =
+    /// File IO Error
+    | FileNotFound of string
+    /// Validation Errors
+    | InvalidGenerationDataHeader of GenerationDataHeaderError
+    | MissingLines of ActualLineCount
+    | MissingColumns of LineIndex
+    | InvalidCharacter of char
+type GenerationData = GenerationData of string list
+type FileLineReader = string -> Result<GenerationData,GenerationError>
+type GenerationDataValidator = GenerationData -> Result<GenerationData,GenerationError>
+type GenerationDataSanitizer = GenerationData -> GenerationData
+type GenerationDataLineSanitizer = string -> string
+type GenerationHeader = { Id: int; LineCount: int; RowCount: int }
+type GenerationInfo = { Header: GenerationHeader; Lines: string list }
+type GenerationHeaderReader = GenerationData -> GenerationHeader
+type GenerationInfoReader = GenerationData -> GenerationInfo
+
+type CellState = Dead | Alive
+type Generation = { Id: int; Cells: CellState [,] }
+type GenerationReader = GenerationInfo -> Generation
+
